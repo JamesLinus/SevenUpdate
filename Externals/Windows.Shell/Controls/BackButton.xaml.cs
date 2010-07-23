108c35b4a55c51def8035d6353632e4e2dc60159
Robert Baker <se7enalive@gmail.com>
2010-07-22 23:07:48 -0700

    Changed header text from 16pt to 14pt and moved position. Added Back button to header and made it function.

---
+++ Externals/Windows.Shell/Controls/BackButton.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Microsoft.Windows.Controls
+{
+    /// <summary>
+    /// Interaction logic for BackButton.xaml
+    /// </summary>
+    public partial class BackButton : UserControl
+    {
+        public BackButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
